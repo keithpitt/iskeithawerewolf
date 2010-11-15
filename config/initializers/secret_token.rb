df683374a31c75b9d475256b4003ff4e908785b8
Keith Pitt <me@keithpitt.com>
2010-11-15 13:17:51 +1030

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iskeithawerewolf::Application.config.secret_token = '57979bbc9b5167fd77b3744bc1379ffd50302fe78bec4ea6cfa2bb764be55eace60b7f273e41339bf9b0251f1a83185ad6657824bfdcee277c032dbe974def88'
